5b4207325c1c2f327ed80f3e2581a9a560ffd015
Equal <amal88@gmail.com>
2016-03-26 01:33:33 -0700

    Much faster implementation that calculates factors first instead of primes. It did not occur to me that calculating factors would be much faster >_<

--- p3/p3.hs
+++ p3/p3.hs
@@ -1,22 +1,26 @@
+import Debug.Trace
+
 {-The prime factors of 13195 are 5, 7, 13 and 29.
  -
  - What is the largest prime factor of the number 600851475143 ?-}
 
-listOfPrimes :: Int -> [Int]
-listOfPrimes x = eraSieve sieve
-    where sieve = [2..x]
+isPrime :: Int -> Bool
+isPrime 1 = True
+isPrime 2 = True
+isPrime 3 = True
+isPrime x
+    | even x = False
+    | any (isFactor x) [y | y <- [3,5..x-1]] = False
+    | otherwise = True
 
-eraSieve :: [Int] -> [Int]
-eraSieve [] = []
-eraSieve (x:xs) = x:remainingPrimes
-    where clearedSieve = [ y | y <- xs, y `mod` x /= 0 ]
-          remainingPrimes = eraSieve clearedSieve
+isFactor :: Int -> Int -> Bool
+isFactor x y = x `mod` y == 0
 
 largestPrimeFactor :: Int -> Int
 largestPrimeFactor x = last primeFactors
     where maxPrime = floor $ sqrt $ fromIntegral x
-          primes = listOfPrimes maxPrime
-          primeFactors = [y | y <- primes, x `mod` y == 0]
+          factors = [y | y <- [1..maxPrime], x `mod` y == 0]
+          primeFactors = [y | y <- factors, isPrime y]
 
 p3 :: Int
 p3 = largestPrimeFactor 600851475143
